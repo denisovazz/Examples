1940ca3b52501a92fe0af237d6bf46d3e067b3a3
Denisova Olga <denisovazz77@gmail.com>
2023-09-25 13:45:40 +0300

    ifElse

---
+++ Example005_UserName/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
