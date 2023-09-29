333a91a2ff628332191403d6e82e9501bd19ee99
Denisova Olga <denisovazz77@gmail.com>
2023-09-29 16:59:49 +0300

    Example 012 Methods

---
+++ Example012_Methods/Program.cs
@@ -0,0 +1,50 @@
+﻿ // Вид 1
+// void Method1()
+// {
+//     System.Console.WriteLine("Автор....");
+// }
+// Method1();
+// Вид 2 
+
+// void Method2(string msg)
+// {
+//     System.Console.WriteLine(msg);
+// }
+// Method2(msg: "Текст сообщения");
+
+// void Method21 (string msg, int count)
+// {
+//     int i = 0;
+//     while (i<count)
+//     {
+//         System.Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21(msg:"Текст",count: 4);
+
+// Вид 3
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// System.Console.WriteLine(year);
+
+// Вид 4
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + c;
+        i++;
+    }
+        return result;
+    
+}
+string res = Method4(10, "fgh");
+System.Console.WriteLine(res);
